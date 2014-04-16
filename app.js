8b667964cb9565eb98a97209e7e90e6a4b553eeb
Kensuke Nagae <kyanny@gmail.com>
2014-04-17 00:58:47 +0900

    Unhighlight continuously by setInterval
    Due to Chrome's sandbox policy, content script cannot listen pjax:success event fired from page js.
    After pjax request, DOM is replaced but $(document).ready event no longer fired again.
    So need to call unhighlight function continuously.

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ $(function(){
     
     color = items.color || "lightgray";
     opacity = items.opacity || 0.7;
-    
-    $('.pulls-list-group li.list-group-item').each(function(index,elem) {
-      if ($(elem).find('.list-group-item-name').text().match(pattern)) {
-        $(elem).css({
-          "background-color": color,
-          "opacity": opacity
-        });
-      }
-    });
+
+    var unhighlighter = function() {
+      $('.pulls-list-group li.list-group-item').each(function(index,elem) {
+        if ($(elem).find('.list-group-item-name').text().match(pattern)) {
+          $(elem).css({
+            "background-color": color,
+            "opacity": opacity
+          });
+        }
+      });
+    };
+
+    unhighlighter();
+    setInterval(unhighlighter, 100);
   });
 });
