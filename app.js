134fea5c17ba261c5354ec86c50108261a77ca7c
Kensuke Nagae <kyanny@gmail.com>
2014-04-19 17:21:23 +0900

    Add case insensitive matching feature

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-chrome.storage.local.get(['pattern', 'color', 'opacity'], function(items) {
-  var pattern, color, opacity;
+chrome.storage.local.get(['pattern', 'case_insensitive', 'color', 'opacity'], function(items) {
+  var pattern, case_insensitive, color, opacity;
+  var re, flags;
 
   pattern = items.pattern;
   if (!pattern) {
     pattern = 'WIP';
   }
-  pattern = new RegExp(pattern, 'i');
+  
+  case_insensitive = !!items.case_insensitive;
+  if (case_insensitive) {
+    flags = 'i';
+  } else {
+    flags = '';
+  }
+
+  re = new RegExp(pattern, flags);
 
   color   = items.color   || 'lightgray';
   opacity = items.opacity || 0.7;
 
-  var unhighlighter = function(element, pattern, color, opacity) {
-    if (element.querySelector('.list-group-item-name').innerText.match(pattern)) {
+  var unhighlighter = function(element, re, color, opacity) {
+    if (element.querySelector('.list-group-item-name').innerText.match(re)) {
       element.style.backgroundColor = color;
       element.style.opacity = opacity;
     }
@@ -29,7 +38,7 @@ chrome.storage.local.get(['pattern', 'color', 'opacity'], function(items) {
         var elements = document.querySelectorAll('.pulls-list-group li.list-group-item');
 
         Array.prototype.forEach.call(elements, function(element) {
-          unhighlighter(element, pattern, color, opacity);
+          unhighlighter(element, re, color, opacity);
         });
       }
     });
