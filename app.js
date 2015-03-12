dd6f0886865134e23e61266285be99bd88301d1c
Kensuke Nagae <kyanny@gmail.com>
2015-03-12 13:37:03 +0900

    Bug fix
    - unhighlighter: querySelector doesn't pick up appropriate anchor element. Use querySelectorAll.
    - Observed event won't happen immediately sometimes. Call walk once to run application as soon as possible.
    - Add matching URL pattern of manifest file.

--- app.js
+++ app.js
@@ -20,11 +20,21 @@ chrome.storage.local.get(['pattern', 'case_insensitive', 'color', 'opacity'], fu
   opacity = items.opacity || 0.7;
 
   var unhighlighter = function(element, re, color, opacity) {
-    if (element.querySelector('.list-group-item-name, .issue-title-link').innerText.match(re)) {
-      element.style.backgroundColor = color;
-      element.style.opacity = opacity;
-    }
+    var items = element.querySelectorAll('.list-group-item-name, .issue-title-link');
+    Array.prototype.filter.call(items, function(item) {
+      if (item.innerText.match(re)) {
+        element.style.backgroundColor = color;
+        element.style.opacity = opacity;
+      }
+    })
   };
+
+  var walk = function(re, color, opacity) {
+    var elements = document.querySelectorAll('.pulls-list-group li.list-group-item, ul.table-list-issues li.table-list-item');
+    Array.prototype.forEach.call(elements, function(element) {
+      unhighlighter(element, re, color, opacity);
+    });
+  }
 
   var target = document.querySelector('body');
   var config = {
@@ -35,14 +45,11 @@ chrome.storage.local.get(['pattern', 'case_insensitive', 'color', 'opacity'], fu
   var observer = new MutationObserver(function(mutations, self) {
     mutations.forEach(function(mutation) {
       if (mutation.type === 'childList') {
-        var elements = document.querySelectorAll('.pulls-list-group li.list-group-item, ul.table-list-issues li.table-list-item');
-
-        Array.prototype.forEach.call(elements, function(element) {
-          unhighlighter(element, re, color, opacity);
-        });
+        walk(re, color, opacity);
       }
     });
   });
 
   observer.observe(target, config);
+  walk(re, color, opacity);
 });
