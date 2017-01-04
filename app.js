a3ef1206300df22397058a6ebdbcbd91f0b3553e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2017-01-04 12:08:17 +0900

    Change selector

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ chrome.storage.local.get(['pattern', 'case_insensitive', 'color', 'opacity'], fu
   opacity = items.opacity || 0.7;
 
   var unhighlighter = function(element, re, color, opacity) {
-    var items = element.querySelectorAll('.Box-row-link');
+    var items = element.querySelectorAll('.Box-row--drag-hide');
     Array.from(items).filter(function(item) {
       if (item.innerText.match(re)) {
         element.style.backgroundColor = color;
